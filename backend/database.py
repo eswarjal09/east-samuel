6071e38a205379a288a09ab9904a3ad6f21a346e
Eswar Balaji <eswarjal09@gmail.com>
2026-01-08 19:36:18 -0800

    disable connection issue

--- backend/database.py
+++ backend/database.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
 from pathlib import Path
 
@@ -17,7 +18,9 @@ if not SQLALCHEMY_DATABASE_URL:
     SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {},
+    poolclass=NullPool if "sqlite" not in SQLALCHEMY_DATABASE_URL else None
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
